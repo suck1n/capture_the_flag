aa2ab1adf9bf444a7fafd542cde96933810471e4
suck1n <duckin@protonmail.com>
2023-04-10 03:23:12 +0200

    Modify cookie lifetime

--- lib/config.ts
+++ lib/config.ts
@@ -2,6 +2,7 @@ export const sessionOptions = {
 	cookieName: "ctf_session",
 	password: process.env.IRON_SESSION_PASSWORD,
 	cookieOptions: {
+		maxAge: undefined,
 		secure: process.env.NODE_ENV === "production",
 	},
 };
