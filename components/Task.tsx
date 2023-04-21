8a66368ea7201495344776c87acf6b21160f8833
suck1n <duckin@protonmail.com>
2023-04-21 11:45:17 +0200

    Refactor Task

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,30 +1,43 @@
 import React, {Component, PropsWithChildren} from "react";
 import styles from "./Task.module.css";
 import MainWrapper from "./MainWrapper";
+import {NextRouter, withRouter} from "next/router";
 
 type TaskProps = {
     title: string
     id: number
-    download: Download
-    url?: string
+    file_names: string[] | string
+    router: NextRouter
 }
 
-type Download = {
-    url: string
-    name: string
-}
+class Task extends Component<PropsWithChildren<TaskProps>, any> {
 
-export default class Task extends Component<PropsWithChildren<TaskProps>, any> {
+    private readonly baseUrl: string;
+    private readonly url: string;
+
+    constructor(props: PropsWithChildren<TaskProps>) {
+        super(props);
+
+        this.baseUrl = typeof window !== "undefined" && (window.location.protocol + "//" + window.location.host) || "";
+        this.url = this.baseUrl + ":" + (5000 + this.props.id);
+    }
+
+    getDownloadUrl(file: string) {
+        return this.baseUrl + "/api/download?file=" + file;
+    }
 
     render() {
         return <MainWrapper>
             <h2>Task {this.props.id}: {this.props.title}</h2>
             {this.props.children}
             <div>
-                {this.props.url && <><b>URL: </b><a className={styles.a} href={this.props.url}>{this.props.url}</a></>}
+                <b>URL: </b><a className={styles.a} href={this.url}>{this.url}</a>
                 <h3>Downloads</h3>
-                <a className={styles.a} href={this.props.download.url}>{this.props.download.name}</a>
+                {((typeof this.props.file_names !== "object" ? [this.props.file_names] : this.props.file_names) as string[])
+                    .map(file => <><a key={file} className={styles.a} href={this.getDownloadUrl(file)}>{file}</a><br/></>)}
             </div>
         </MainWrapper>;
     }
 }
+
+export default withRouter(Task);
