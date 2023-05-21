41aaba1f62eba744391c9abf6878b131f8d312de
suck1n <duckin@protonmail.com>
2023-05-21 21:46:17 +0200

    Hotfix Task

--- components/Task.tsx
+++ components/Task.tsx
@@ -34,7 +34,7 @@ class Task extends Component<PropsWithChildren<TaskProps>, any> {
             <h2>Task {this.props.id}: {this.props.title}</h2>
             {this.props.children}
             <div>
-                <b>URL: </b><a className={styles.a} href={this.url}>{this.url}</a>
+                <b>URL: </b><a className={styles.a} href={this.props.path ? "" : this.url}>{this.url}</a>
                 {typeof this.props.file_names !== "undefined" && <h3>Downloads</h3>}
                 {typeof this.props.file_names !== "undefined" && (((typeof this.props.file_names !== "object" ? [this.props.file_names] : this.props.file_names) as string[])
                     .map(file => <><a key={file} className={styles.a} href={this.getDownloadUrl(file)}>{file}</a><br/></>))}
