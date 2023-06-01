edf587060471d394fca319820ae1dfbb8c8a4276
linuxadmin <linuxadmin@debian11.tfo.bx>
2023-06-01 11:19:43 +0200

    Add YT-Link to Explain DH-Exchange

--- pages/tasks/4.tsx
+++ pages/tasks/4.tsx
@@ -14,6 +14,7 @@ export default class ManInTheMiddleAttack extends Component<any, any> {
                 <p>Das Server Skript stellt eine Verbindung zwischen zwei Parteien her, und leitet deren Nachrichten unverändert weiter. Zur sicheren Kommunikation vereinbaren beide Parteien zunächst mit Hilfe des Diffie-Hellman-Verfahrens einen gemeinsamen Schlüssel k.</p>
                 <p>Im Anschluss schickt Partei A mit AES-CBC verschlüsselte Nachrichten an Partei B. Der verwendete Schlüssel besteht dabei aus den ersten 16 Bytes des SHA-512-Hashes von k. Für bessere Sicherheit verwenden die beiden Parteien HMAC-SHA256 als Integritätsschutz (allerdings im MAC-then-Encrypt-Modus) sowie einen zufälligen IV, der jeweils der Nachricht vorangestellt wird.</p>
                 <p><strong>Hinweis:</strong> Nutze für die Operation k entweder die Python-Funktion <code>pow(a, b, p)</code> oder die Java-Funktion <code>new BigInteger(a).modPow(b, p)</code>.</p>
+		<p><strong>Hinweis:</strong> Sieh dir dieses Video für eine Erklärung des DH-Algorithmuses an: <a href={"https://www.youtube.com/watch?v=3QnD2c4Xovk"}>https://www.youtube.com/watch?v=3QnD2c4Xovk</a></p>
             </Task>
         );
     }
